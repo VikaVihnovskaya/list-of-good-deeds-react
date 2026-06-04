03f810b173d12ed30bfffa4d5382860ed191a849
VikaVihnovskaya <vihnovskaya1932@gmail.com>
2026-06-04 20:19:05 +0300

    feat: add full user logic to backend for CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DeedsModule } from './deeds/deeds.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +23,8 @@ import { DeedsModule } from './deeds/deeds.module';
       }),
     }),
     DeedsModule,
+    UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
